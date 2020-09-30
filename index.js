6c72e1e6b5261818b02a4129ca2a70660a09a700
Aaron Abramov <dabramov@fb.com>
2020-09-30 14:01:54 -0500

    1.0.1

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
-const mock = require('./rules/mock.js')
+const mock = require("./rules/mock.js");
 
 const allRules = {
-  'extra-arrow-initializer': mock,
-  'no-console': mock,
-  'no-new-error': mock,
-  'no-symbol': mock,
-  'react-hooks-deps': mock,
-  'react-hooks': mock,
-  'react-no-useless-fragment': mock,
-}
+  "extra-arrow-initializer": mock,
+  "no-console": mock,
+  "no-new-error": mock,
+  "no-symbol": mock,
+  "react-hooks-deps": mock,
+  "react-hooks": mock,
+  "react-no-useless-fragment": mock,
+};
 
 module.exports = {
   rules: allRules,
   configs: {
     recommended: {
-      plugins: [
-        'fb-www'
-      ],
+      plugins: ["fb-www"],
       parserOptions: {
         ecmaFeatures: {
-          jsx: true
-        }
+          jsx: true,
+        },
       },
       rules: {
-        'fb-www/extra-arrow-initializer': 0
-        'fb-www/no-console': 0,
-        'fb-www/no-new-error': 0 
-        'fb-www/no-symbol': 0,
-        'fb-www/react-hooks-deps': 0,
-        'fb-www/react-hooks': 0,
-        'fb-www/react-no-useless-fragment': 0,
-      }
+        "fb-www/extra-arrow-initializer": 0,
+        "fb-www/no-console": 0,
+        "fb-www/no-new-error": 0,
+        "fb-www/no-symbol": 0,
+        "fb-www/react-hooks-deps": 0,
+        "fb-www/react-hooks": 0,
+        "fb-www/react-no-useless-fragment": 0,
+      },
     },
     all: {
-      plugins: [
-        'fb-www'
-      ],
+      plugins: ["fb-www"],
       parserOptions: {
         ecmaFeatures: {
-          jsx: true
-        }
-      }
-    }
-  }
+          jsx: true,
+        },
+      },
+    },
+  },
 };
